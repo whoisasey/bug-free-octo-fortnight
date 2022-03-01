68fb851595fa574f10d93c7aaed302baa91197e2
whoisasey <atinachang@gmail.com>
2022-03-01 11:25:38 -0500

    add articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+
   def index
     @articles = Article.all
   end
@@ -7,4 +7,34 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if  @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root "pages#index"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:index, :show]
+  resources :articles, only: [:index, :show, :create, :new, :edit, :update]
 end
