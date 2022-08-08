9096d7976d334a64b8a3881ab687e786988e707f
whoisasey <atinachang@gmail.com>
2022-08-08 11:52:32 -0400

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root "pages#index"
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
