a0b94bb8246c7a13c2092c81d8f2620ffc7e5993
whoisasey <atinachang@gmail.com>
2022-08-10 12:28:34 -0400

    add users index page, image placeholder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-
+  
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
+  end
+  
+  def index
+    @users = User.all
   end
 
   def new
